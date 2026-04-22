b46a150ac46c2506381df6a8668e36cfb71c5f37
Oskar Skowroński <skowronskioskar372@gmail.com>
2026-04-22 22:04:53 +0200

    Pomiar z wykorzystaniem AR
    Dodano podstawowy kod pomiaru odległości z wykorzystaniem AR

--- Assets/Scripts/ArRangefinder.cs
+++ Assets/Scripts/ArRangefinder.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 public class ArRangefinder : MonoBehaviour
 {
@@ -12,9 +13,11 @@ public class ArRangefinder : MonoBehaviour
     public TextMeshProUGUI distanceText;
 
     [Header("Komponenty dodatkowe")]
-    public Image Crosshair;
+    public Image crosshair;
 
     private static List<ARRaycastHit> hits = new List<ARRaycastHit>(); // Lista do przechowywania wyników
+
+    private float smoothedDistance = 0f; // Zmienna do przechowywania wygładzonej odległości
 
     void Start()
     {
@@ -26,9 +29,20 @@ public class ArRangefinder : MonoBehaviour
     {
         Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f); // Wyliczenie środka ekranu
 
-        if (raycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon | UnityEngine.XR.ARSubsystems.TrackableType.FeaturePoint))
+        if (raycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon | TrackableType.FeaturePoint))
         {
+            Pose hitPose = hits[0].pose;
+
+            float actualDistance = Vector3.Distance(arCamera.transform.position, hitPose.position);
+
+            smoothedDistance = Mathf.Lerp(smoothedDistance, actualDistance, Time.deltaTime * 10f);
+
+            distanceText.text = $"Dystancs:\n{smoothedDistance.ToString("F2")} m";
             
+        }
+        else
+        {
+            distanceText.text = "Szukam celu...";
         }
     }
 }
